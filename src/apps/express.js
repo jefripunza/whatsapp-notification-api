1a0a8939c9d5272450383e89b49e51e9b7199ba9
Jefri Herdi Triyanto <jefripunza@gmail.com>
2023-04-22 22:54:30 +0700

    frontend change to react

--- src/apps/express.js
+++ src/apps/express.js
@@ -1,18 +1,25 @@
+const path = require("path");
+
 const express = require("express");
 const http = require("http");
-const { temp_dir } = require("../config");
+const { frontend_dir } = require("../config");
 
 const app = express();
 const server = http.createServer(app);
 
 app.use(require("cors")());
 app.use(require("helmet")());
+
+// frontend view
+app.use(express.static(frontend_dir));
+app.get("/", (_, res) => res.sendFile(path.join(frontend_dir, "index.html")));
+
+// api
 app.use(require("morgan")("dev"));
-
 require("../module")(app);
-
-app.use(express.static(temp_dir));
 app.get("*", (_, res) => res.status(404).json({ message: "page not found!" }));
-app.all("*", (_, res) => res.status(404).json({ message: "endpoint not found!" }));
+app.all("*", (_, res) =>
+  res.status(404).json({ message: "endpoint not found!" })
+);
 
 module.exports = { app, server };
--- src/config.js
+++ src/config.js
@@ -3,9 +3,11 @@ const path = require("path");
 require("dotenv").config();
 
 exports.PORT = process.env.PORT ?? 5000;
+
 exports.temp_dir = path.join(process.cwd(), "temp");
-exports.frontend_dir = path.join(process.cwd(), "frontend", "build");
 // create temp directory
 if (!fs.existsSync(this.temp_dir)) {
   fs.mkdirSync(this.temp_dir);
 }
+
+exports.frontend_dir = path.join(process.cwd(), "frontend", "dist");
