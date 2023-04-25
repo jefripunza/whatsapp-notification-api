e67d731eb33f4c4163705d2313ef0ec33e92b82c
Jefri Herdi Triyanto <jefripunza@gmail.com>
2023-04-25 13:29:30 +0700

    remove testing file

--- gptt.js
+++
@@ -1,8 +0,0 @@
-const str =
-  "this is example string to match #anu# value or #ini# and # itu# value";
-
-const result = String(str)
-  .match(/#(\w+)#/g)
-  .map((match) => String(match).replace(/#/g, ""));
-
-console.log({ result });
